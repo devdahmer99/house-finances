6f84a3f78fd7ab7ae835fd03559bab475a00f954
Eduardo Correa <ecorrea@gvdasa.com.br>
2024-12-03 15:29:38 -0300

    feat: adicionado no projeto de infra, a camada de contexto para comunicar com o banco de dados e também foi adicionado dentro do program.cs dentro do projeto de api a chamada da classe do dbcontext e referenciado a nossa classe para geração de migrations e afins.

--- financesFlow/src/financesFlow.API/Program.cs
+++ financesFlow/src/financesFlow.API/Program.cs
@@ -1,3 +1,6 @@
+using financesFlow.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<financesFlowDbContext>(options => options.UseSqlite("Data Source=financesFlow.db"));
 
 var app = builder.Build();
 
---
+++ financesFlow/src/financesFlow.Infra/Context/financesFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using financesFlow.Aplicacao.Entidades.Despesa;
+using Microsoft.EntityFrameworkCore;
+
+namespace financesFlow.Infra.Context
+{
+    public class financesFlowDbContext : DbContext
+    {
+        public financesFlowDbContext(DbContextOptions<financesFlowDbContext> options) : base(options)
+        { }
+        
+        public DbSet<Despesa> Despesas { get; set; }
+    }
+}
