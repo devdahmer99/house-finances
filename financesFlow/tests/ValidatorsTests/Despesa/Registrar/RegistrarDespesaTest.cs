fa995d634f56c3f2a5aae3facfb1c8d36114680e
Eduardo Dahmer <Eduardodahmer99@outlook.com>
2024-12-07 20:32:13 -0300

    feat: Iniciando a implementação dos testes unitarios na aplicação e validando o Validator de despesas.

---
+++ financesFlow/tests/ValidatorsTests/Despesa/Registrar/RegistrarDespesaTest.cs
@@ -0,0 +1,24 @@
+﻿using financesFlow.Aplicacao.useCase.Despesa.Registrar;
+using financesFlow.Comunicacao.Requests;
+
+namespace ValidatorsTests.Despesa.Registrar;
+public class RegistrarDespesaTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegistrarValidacaoDespesa();
+        var request = new RequestDespesaJson 
+        {
+            ValorDespesa = 100,
+            NomeDespesa = "Teste",
+            DescricaoDespesa = "despesa de teste",
+            FormaDePagamento = financesFlow.Comunicacao.Enums.MetodoPagamento.Pix,
+            DataDespesa = DateTime.Now.AddDays(-1)
+        };
+
+        var result = validator.Validate(request);
+
+        Assert.True(result.IsValid);
+    }
+}
