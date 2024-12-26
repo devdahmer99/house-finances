4724da06bc21c218fdf5728621de1c8bd9e116f9
Eduardo Dahmer <Eduardodahmer99@outlook.com>
2024-12-26 14:21:15 -0300

    hotfix: Correção do bug que não exibia o valor das despesas na requisição, ao invés de exibir o valor da despesa exibia zero no lugar, ajustado nos responses o nome da propriedade, alterado de Valor para ValorDespesa para assim exibir o valor corretamente.

--- financesFlow/src/financesFlow.Comunicacao/Responses/ResponseDespesaJson.cs
+++ financesFlow/src/financesFlow.Comunicacao/Responses/ResponseDespesaJson.cs
@@ -6,7 +6,7 @@ public class ResponseDespesaJson
     public string NomeDespesa { get; set; } = string.Empty;
     public string Descricao { get; set; } = string.Empty;
     public DateTime Data { get; set; }
-    public decimal Valor { get; set; }
+    public decimal ValorDespesa { get; set; }
     public MetodoPagamento Pagamento { get; set; }
 
 }
--- financesFlow/src/financesFlow.Comunicacao/Responses/ResponseShortDepesaJson.cs
+++ financesFlow/src/financesFlow.Comunicacao/Responses/ResponseShortDepesaJson.cs
@@ -3,6 +3,6 @@ public class ResponseShortDepesaJson
 {
     public long Id { get; set; }
     public string NomeDespesa { get; set; } = string.Empty;
-    public decimal Valor { get; set; }
+    public decimal ValorDespesa { get; set; }
 
 }
