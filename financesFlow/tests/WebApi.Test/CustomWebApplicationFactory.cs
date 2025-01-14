a4b169530af20681a2c5b324cf11eede90436f9c
Eduardo Correa <ecorrea@gvdasa.com.br>
2025-01-14 10:34:01 -0300

    hotfix: Ajuste na função StarDatabase, onde é utilizado o objeto de usuario, pois o sistema estava lançando uma SystemArgumentException, pois o objeto estava vazio, devido a utilização de uma variável local incorreta ao invés do próprio objeto _user. Também foi removido a classe JwtTokenSettings que não é mais utilizada.

--- financesFlow/tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ financesFlow/tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.ConstrainedExecution;
+using CommonTestsUtilitis.Entidades;
+using financesFlow.Dominio.Seguranca.Criptografia;
 using financesFlow.Infra.DataAccess;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -9,6 +11,8 @@ namespace WebApi.Test
 {
     public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
+        private financesFlow.Dominio.Entidades.Usuario _user;
+        private string _password;
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             
@@ -22,7 +26,31 @@ namespace WebApi.Test
                         config.UseInMemoryDatabase("InMemoryDbForTesting");
                         config.UseInternalServiceProvider(provider);
                     });
+
+                    var scope = services.BuildServiceProvider().CreateAsyncScope();
+                    var dbContext = scope.ServiceProvider.GetRequiredService<financesFlowDbContext>();
+                    var encriptador = scope.ServiceProvider.GetRequiredService<IEncriptadorSenha>();
+
+                    StartDatabase(dbContext, encriptador);
                 });
+        }
+        public string getNome() => _user.Nome;
+
+        public string getEmail() => _user.Email;
+
+        public string getPassword() => _password;
+
+        private void StartDatabase(financesFlowDbContext dbContext, IEncriptadorSenha encriptador)
+        {
+            _user = UserBuilder.Build();
+
+            _password = _user.Senha;
+
+            _user.Senha = encriptador.Encript(_user.Senha);
+
+            dbContext.Usuarios.Add(_user);
+
+            dbContext.SaveChanges();
         }
     }
 }
---
+++ financesFlow/tests/WebApi.Test/Usuarios/DoLogin/FazLoginUsuarioTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using financesFlow.Comunicacao.Requests.Usuario;
+using financesFlow.Comunicacao.Responses.Usuario;
+using FluentAssertions;
+
+namespace WebApi.Test.Usuarios.DoLogin
+{
+    public class FazLoginUsuarioTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private const string METHOD = "api/login/efetualogin";
+        private readonly HttpClient _httpClient;
+        private readonly string _email;
+        private readonly string _nome;
+        private readonly string _senha;
+        public FazLoginUsuarioTest(CustomWebApplicationFactory webApplicationFactory)
+        {
+            _httpClient = webApplicationFactory.CreateClient();
+            _nome = webApplicationFactory.getNome();
+            _email = webApplicationFactory.getEmail();
+            _senha = webApplicationFactory.getPassword();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = new RequestLoginUsuarioJson
+            {
+                Email = _email,
+                Senha = _senha
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            responseData.RootElement.GetProperty("nome").GetString().Should().Be(_nome);
+            responseData.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
